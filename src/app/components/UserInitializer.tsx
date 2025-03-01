031b2591610d5204544adbe3ad93404950ec94e6
Apurvborhade <apurvaborhadee@gmail.com>
2025-03-01 21:33:16 +0530

    redirect issue

--- src/app/components/UserInitializer.tsx
+++ src/app/components/UserInitializer.tsx
@@ -5,6 +5,7 @@ import { RootState } from '@/app/store'
 import { useDispatch, useSelector } from 'react-redux'
 import React from 'react'
 import { redirect, usePathname } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 
 const UserInitializer = ({
     user
@@ -12,12 +13,14 @@ const UserInitializer = ({
     user: unknown
 }>) => {
     const pathname = usePathname()
-
+    const { data: userLogged, status } = useSession();
     React.useEffect(() => {
-        if (!user && pathname !== '/auth/signin' && pathname !== '/auth/signup' && pathname !== '/' && !pathname.startsWith('/matches/match-request')) {
-            redirect('/auth/signin')
+        if (status === "loading") return; // Avoid redirecting before session loads
+
+        if (!userLogged && pathname !== "/auth/signin" && pathname !== "/auth/signup" && pathname !== "/" && !pathname.startsWith("/matches/match-request")) {
+            redirect("/auth/signin");
         }
-    }, [user,pathname])
+    }, [userLogged, status, pathname]);
     const { user: alreadyUserExist } = useSelector((state: RootState) => state.user)
 
     const dispatch = useDispatch()
